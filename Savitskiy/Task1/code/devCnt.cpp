d4568cc4a88a51a1be4857510b3585985d364183
AACprog <145167289+AACprog@users.noreply.github.com>
2024-10-15 17:23:42 +0300

    Update devCnt.cpp

--- Savitskiy/Task1/code/devCnt.cpp
+++ Savitskiy/Task1/code/devCnt.cpp
@@ -1,13 +1,11 @@
 #include "header.hpp"
 
 
-std::tuple<double, double, double> answerVariation(
+std::tuple<double, double> answerVariation(
     const std::vector<std::vector<double>>& matrix,
     const std::vector<LinearEquation>& equations,
     const std::vector<double>& solution
 ) {
-    clock_t start = clock();
-    
     double b = 0, AxbNorm = 0;
     double r_up2 = 0, r_down2 = 0;
     for (size_t i = 0; i < matrix.size(); ++i) {
@@ -15,19 +13,19 @@ std::tuple<double, double, double> answerVariation(
         for (size_t j = 0; j < matrix.size(); ++j) {
             AxbRow += solution[j] * matrix[i][j];
         }
-        AxbNorm += std::abs(AxbRow - matrix[i][matrix.size() - 1]);
+        AxbNorm += std::abs(AxbRow - matrix[i][matrix.size()]);
     }
     for (size_t i = 0; i < equations.size(); ++i) {
-        b += std::abs(matrix[i][matrix.size() - 1]);    
-        r_up2 += std::abs(equations[i].FreeCoefficient - double(equations[i].Variables[i].index % 2));
-        r_down2 += double(equations[i].Variables[i].index % 2);
+        b += std::abs(matrix[i][matrix.size()]);    
+        r_up2 += std::abs(equations[i].FreeCoefficient - double((equations[i].Variables[i].index + 1) % 2));
+        r_down2 += double((equations[i].Variables[i].index + 1) % 2);
 
     }
     for (size_t i = equations.size(); i < matrix.size(); ++i) {
-        b += std::abs(matrix[i][matrix.size() - 1]);
+        b += std::abs(matrix[i][matrix.size()]);
     }
 
     double r1 = AxbNorm / b;
     double r2 = r_up2 / r_down2;
-    return std::make_tuple(r1, r2, (double)(clock() - start) / CLOCKS_PER_SEC);
+    return std::make_tuple(r1, r2);
 }
