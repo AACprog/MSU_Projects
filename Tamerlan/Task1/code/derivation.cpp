ea04b6520af8a31af46287ee2c398d70582d1d4e
AACprog <145167289+AACprog@users.noreply.github.com>
2024-10-17 23:36:45 +0300

    Create derivation.cpp

---
+++ Tamerlan/Task1/code/derivation.cpp
@@ -0,0 +1,31 @@
+#include "header.hpp"
+
+
+std::tuple<double, double> answerVariation(
+    const std::vector<std::vector<double>>& matrix,
+    const std::vector<LinearEquation>& equations,
+    const std::vector<double>& solution
+) noexcept {
+    double b = 0, AxbNorm = 0;
+    double r_up2 = 0, r_down2 = 0;
+    for (size_t i = 0; i < matrix.size(); ++i) {
+        double AxbRow = 0;
+        for (size_t j = 0; j < matrix.size(); ++j) {
+            AxbRow += solution[j] * matrix[i][j];
+        }
+        AxbNorm += std::abs(AxbRow - matrix[i][matrix.size()]);
+    }
+    for (size_t i = 0; i < equations.size(); ++i) {
+        b += std::abs(matrix[i][matrix.size()]);    
+        r_up2 += std::abs(solution[i] - double((i + 1) % 2));
+        r_down2 += double((i + 1) % 2);
+
+    }
+    for (size_t i = equations.size(); i < matrix.size(); ++i) {
+        b += std::abs(matrix[i][matrix.size()]);
+    }
+
+    double r1 = AxbNorm / b;
+    double r2 = r_up2 / r_down2;
+    return std::make_tuple(r1, r2);
+}
